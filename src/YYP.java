2112e61fb8cff4ef4b6127ac99484d62b067924a
KayWen <KayWen@XJW>
2016-11-09 16:02:30 +0800

    YYP档案加上是否已经结婚

--- src/YYP.java
+++ src/YYP.java
@@ -10,15 +10,16 @@ public class YYP implements Serializable{
 	private static final long serialVersionUID = 1L;
 		private int age;
 		private String name;
-		
+		private boolean isMarry;
 		public YYP() {
 			// TODO Auto-generated constructor stub
 		}
 
-		public YYP(int age, String name) {
+		public YYP(int age, String name, boolean isMarry) {
 			super();
 			this.age = age;
 			this.name = name;
+			this.isMarry = isMarry;
 		}
 
 		public int getAge() {
@@ -35,6 +36,14 @@ public class YYP implements Serializable{
 
 		public void setName(String name) {
 			this.name = name;
+		}
+
+		public boolean isMarry() {
+			return isMarry;
+		}
+
+		public void setMarry(boolean isMarry) {
+			this.isMarry = isMarry;
 		}
 		
 		
